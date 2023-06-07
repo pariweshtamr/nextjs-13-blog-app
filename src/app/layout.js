4458a64182de16044dbda7b8b569a768c1a55c07
pariwesh <tamrpariwesh@gmail.com>
2023-06-07 13:57:02 +1000

    deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,6 @@ import Navbar from "@/components/navbar/Navbar"
 import "./globals.css"
 import Footer from "@/components/footer/Footer"
 import { Providers } from "@/app/redux/Provider"
-import { Inter } from "next/font/google"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Next Blog App",
@@ -14,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${inter.className} min-h-[100vh] flex flex-col`}>
+      <body className="min-h-[100vh] flex flex-col">
         <Providers>
           <Navbar />
           <main className="grow shrink basis-auto">{children}</main>
